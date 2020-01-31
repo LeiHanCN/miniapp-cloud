c7fbc0aef0c708e651715ff0f0b612a8aea86d8b
leihan <han.lei@mail.hypers.com>
2020-01-31 14:38:27 +0800

    修正声明

--- types/App.d.ts
+++ types/App.d.ts
@@ -9,7 +9,7 @@ type AppConstructorOptions =
     }
 
 interface App {
-  route: (path: string, handler: RouteHandler | Function) => void
+  route: (path: string, handler: TRouteHandler = RouteHandler) => void
   handle: (event: RouteHandlerEvent, context: RouteHandlerContext) => void
 }
 interface AppConstructor {
